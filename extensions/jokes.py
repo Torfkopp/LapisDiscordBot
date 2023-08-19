ff3925fd6eb9c43ef551662614d655b984287ce2
Mario <brickforce4@gmail.com>
2023-08-19 15:50:03 +0200

    Dad jokes hinzugefügt

---
+++ extensions/jokes.py
@@ -0,0 +1,49 @@
+import random
+
+import requests
+from interactions import (
+    Extension, OptionType, slash_option, slash_command, SlashContext
+)
+
+""" File for the joke commands """
+
+
+def setup(bot): Jokes(bot)
+
+
+class Jokes(Extension):
+    @slash_command(name="dad_joke", description="Gibt einen zufälligen Dad Joke zurück")
+    @slash_option(
+        name="theme_option",
+        description="Thema des Witzes auf Angelsächsisch (Random, wenn nix gefunden)",
+        required=False,
+        opt_type=OptionType.STRING
+    )
+    async def dad_joke_function(self, ctx: SlashContext, theme_option: str = ""):
+        await ctx.send(get_dad_joke(theme_option))
+
+
+def get_dad_joke(term):
+    """ Returns a random dad joke or one fitting the theme """
+    url = "https://icanhazdadjoke.com/"
+    payload = ""
+    headers = {"Accept": "application/json"}
+    joke = ""
+
+    if term != "":
+        url_term = url + f"search?term={term}"
+        response = requests.request("GET", url_term, data=payload, headers=headers)
+        response = response.json()
+
+        jokes = response['results']
+        if len(jokes) == 0: joke = ""
+        elif len(jokes) == 1: joke = jokes[0]['joke']
+        else: joke = jokes[random.randint(0, len(jokes)-1)]['joke']
+
+    if joke == "":
+        response = requests.request("GET", url, data=payload, headers=headers)
+
+        joke = response.json()
+        joke = joke["joke"]
+
+    return joke
