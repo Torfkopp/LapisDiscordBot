3d331ee5fcfb57f2637f993d275bd910d6ac0e80
Mario <brickforce4@gmail.com>
2023-09-03 22:11:41 +0200

    BotActivity, ErrorHandling added + Denkfehler fix

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import datetime
+import traceback
 
 import interactions
 from interactions import Client, Intents, listen, Task, IntervalTrigger, DateTrigger
+from interactions.api.events import Error
 from interactions.client.errors import HTTPException
+from interactions.ext import prefixed_commands
 from interactions.models import discord
 from matplotlib import font_manager
 
@@ -12,7 +15,8 @@ from extensions import freegames, lolesport, lol_patchnotes
 from extensions.football import football
 from extensions.formula1 import formula1
 
-bot = Client(intents=Intents.DEFAULT)
+bot = Client(intents=Intents.DEFAULT, send_command_tracebacks=False)
+prefixed_commands.setup(bot)
 
 LIVE_SCORE_MESSAGE = ""
 current_task = None
@@ -20,12 +24,60 @@ current_task = None
 TEST_MODE_ON = False
 
 
-async def change_activity(activity):
-    """ Changes the bot's presence accordingly
-    activity: the activity the Bot should do (note "Watching" comes before the string) """
-    await bot.change_presence(status=discord.Status.IDLE,
-                              activity=discord.activity.Activity.create(name=activity,
-                                                                        type=discord.activity.ActivityType.WATCHING))
+class ActivityClass:
+    football_schedule = []
+    formula1_schedule = []
+    # TODO Hier einiges hinpacken
+    # "GAME" (Spielt), "STREAMING" (Streamt), "LISTENING" (Hört X zu), "WATCHING" (Schaut), "COMPETING" (Tritt an)
+    activities = [("sich Bücher an", discord.activity.ActivityType.WATCHING),
+                  ("sich Anime an", discord.activity.ActivityType.WATCHING)]
+    status = discord.Status.IDLE  # possible: "ONLINE", "OFFLINE", "DND", "IDLE", "INVISIBLE"
+
+    def __init__(self, football_schedule, formula1_schedule):
+        self.football_schedule = football_schedule
+        self.formula1_schedule = formula1_schedule
+
+    async def test_mode(self):
+        self.status = discord.Status.DND
+        await self._change_activity("im Test Modus", discord.activity.ActivityType.COMPETING)
+
+    async def rotate_activity(self):
+        """ Changes the activity depending on the situation """
+        now = datetime.datetime.now()
+        watches_football, watches_formula1 = False, False
+        for time in self.football_schedule:
+            if datetime.timedelta(minutes=0) < now - time < datetime.timedelta(minutes=100): watches_football = True
+        for time in self.formula1_schedule:
+            if datetime.timedelta(minutes=0) < now - time < datetime.timedelta(minutes=100): watches_formula1 = True
+        watches_football = True
+        if watches_football and watches_formula1:
+            self.status = discord.Status.DND
+            name = "Fußball und Formel 1"
+            type_ = discord.activity.ActivityType.WATCHING
+        elif watches_football:
+            self.status = discord.Status.DND
+            name = "sich Fußball an"
+            type_ = discord.activity.ActivityType.WATCHING
+        elif watches_formula1:
+            self.status = discord.Status.DND
+            name = "sich Formel 1 an"
+            type_ = discord.activity.ActivityType.WATCHING
+        else:
+            self.status = discord.Status.IDLE
+            activity = self.activities.pop()  # Take first element and put it at the end
+            self.activities.insert(0, activity)
+            name = activity[0]
+            type_ = activity[1]
+
+        await self._change_activity(name, type_)
+        return
+
+    async def _change_activity(self, activity_name, activity_type):
+        """ Internal method to chance the activity"""
+        print(activity_name, activity_type)
+        activity = discord.activity.Activity.create(name=activity_name, type=activity_type)
+        await bot.change_presence(status=self.status, activity=activity)
+        return
 
 
 @Task.create(IntervalTrigger(minutes=1))
@@ -36,10 +88,8 @@ def reduce_command_calls():
     lolesport.reduce_command_calls()
 
 
-# noinspection PyUnresolvedReferences
 async def live_scoring():
     """ Sends the live scoring to the channel and keeps it updated until all live games have ended """
-    await change_activity("sich Fußball an")
     global LIVE_SCORE_MESSAGE
     if LIVE_SCORE_MESSAGE == "":
         LIVE_SCORE_MESSAGE = await bot.get_channel(util.SPORTS_CHANNEL_ID).send(embeds=football.get_live()[0])
@@ -50,9 +100,9 @@ async def live_scoring():
         except HTTPException:
             LIVE_SCORE_MESSAGE = await bot.get_channel(util.SPORTS_CHANNEL_ID).send(embeds=embeds)
         if not still_going:
+            # noinspection PyUnresolvedReferences
             current_task.stop()  # current_task is a task when this is called, thus PyUnresolvedReferences ignorable
             print("Live scoring stops")
-            await change_activity("sich wieder Bücher an")
 
 
 async def start_gip():
@@ -71,11 +121,10 @@ async def start_gip():
 
 async def formula1_result():
     """ When called, sends in the result of the latest Formula1 session"""
-    await change_activity("sich Formel1 Highlights an")
     result = formula1.auto_result()
     # If getting result fails, try again in an hour
-    if result == "": Task(formula1_result, DateTrigger(datetime.datetime.now() + datetime.timedelta(hours=1))).start()
-    await bot.get_channel(util.SPORTS_CHANNEL_ID).send(result)
+    if result == "": Task(formula1_result, DateTrigger(datetime.datetime.now() + datetime.timedelta(hours=0.5))).start()
+    else: await bot.get_channel(util.SPORTS_CHANNEL_ID).send(result)
 
 
 async def update_patchnotes():
@@ -84,12 +133,17 @@ async def update_patchnotes():
     if embed: await bot.get_channel(util.LABAR_CHANNEL_ID).send(embed=embed)
 
 
+@listen(Error, disable_default_listeners=True)
+async def on_error(event: Error):
+    traceback.print_exception(event.error)
+    await event.ctx.send(embed=util.get_error_embed("error"))
+
+
 @listen()
 async def on_ready():
     """ Is called when the bot is ready """
     print("Ready")
     print(f"This bot is owned by {bot.owner}")
-    await change_activity("sich Bücher an")
     # Loads the Formula1 font
     for font in font_manager.findSystemFonts(["formula1/font"]): font_manager.fontManager.addfont(font)
 
@@ -97,7 +151,10 @@ async def on_ready():
 @listen()
 async def on_startup():
     """ Is called when the bot starts up (used for schedule things) """
-    if TEST_MODE_ON: return
+    if TEST_MODE_ON:
+        activity = ActivityClass([], [])
+        await activity.test_mode()
+        return
     reduce_command_calls.start()
 
     # FOOTBALL LIVE SCORING PART
@@ -105,7 +162,8 @@ async def on_startup():
     print("Starting times of today's games: " + str(football_schedule))
     for start_time in football_schedule:
         # When the start time was less than 90 minutes ago, start the live scoring automatically
-        if start_time - datetime.datetime.now() > datetime.timedelta(minutes=-90): await start_gip()
+        if datetime.timedelta(minutes=0) < (datetime.datetime.now() - start_time) < datetime.timedelta(minutes=90):
+            await start_gip()
         task = Task(start_gip, DateTrigger(start_time))
         task.start()
 
@@ -113,9 +171,12 @@ async def on_startup():
     formula1_schedule, embed = formula1.create_schedule()
     if isinstance(embed, interactions.Embed): await bot.get_channel(util.SPORTS_CHANNEL_ID).send(embed=embed)
     print("Today's formula1 sessions: " + str(formula1_schedule))
-    for start_time in formula1_schedule:
-        task = Task(formula1_result, DateTrigger(start_time))
-        task.start()
+    for start_time in formula1_schedule: Task(formula1_result, DateTrigger(start_time)).start()
+
+    # AUTOMATIC ACTIVITY CHANGE PART
+    activity = ActivityClass(football_schedule, formula1_schedule)
+    await activity.rotate_activity()
+    Task(activity.rotate_activity, IntervalTrigger(minutes=2)).start()
 
     # AUTOMATIC FREE GAMES PART
     if datetime.datetime.now().weekday() == 4:
