f5cdeb084314a09f78d9bac12678069db958e9df
Mario <brickforce4@gmail.com>
2023-09-05 23:17:52 +0200

    Help in Prefixed integriert

--- extensions/prefixed.py
+++ extensions/prefixed.py
@@ -2,6 +2,8 @@ import interactions
 from interactions import Extension
 from interactions.ext.prefixed_commands import prefixed_command, PrefixedContext
 
+import util
+
 
 def setup(bot): Prefixed(bot)
 
@@ -10,15 +12,34 @@ COLOUR = util.Colour.PREFIXED.value
 
 
 class Prefixed(Extension):
-    @prefixed_command(name="hello")
-    async def hello_function(self, ctx: PrefixedContext):
-        await ctx.reply(embed=answers())
+    @prefixed_command()
+    async def hello(self, ctx: PrefixedContext): await ctx.reply(embed=hello())
+
+    @prefixed_command()
+    async def hallo(self, ctx: PrefixedContext): await ctx.reply(embed=hello())
+
+    @prefixed_command()
+    async def moin(self, ctx: PrefixedContext): await ctx.reply(embed=hello())
+
+    @prefixed_command(name="help")
+    async def help(self, ctx: PrefixedContext): await ctx.reply(embed=help_())
+
+    @prefixed_command()
+    async def hilfe(self, ctx: PrefixedContext): await ctx.reply(embed=help_())
 
 
-def answers():
-    #  TODO hier Krams
-    gif = "https://media.giphy.com/media/s1tAtvJLIYiVWNaCqy/giphy-downsized-large.gif"
-    text = "Gomenasorry, something wrong"
-    embed = interactions.Embed(title=text)
-    embed.set_image(url=gif)
+def hello():
+    embed = interactions.Embed(title="Hallo zurück!", color=COLOUR)
+    embed.set_image(url=util.get_gif("hello"))
     return embed
+
+
+def help_():
+    embed = interactions.Embed(title="Lapis hilft dir gerne! :)", color=COLOUR,
+                               thumbnail="https://raw.githubusercontent.com/Torfkopp/LapisDiscordBot/master/resources/Lapis2.jpg")
+    embed.description = ("Für eine Auflistung all meiner Befehle einfach / in den Chat und durchscrollen.\n"
+                         "Ansonsten gibt es hier nicht viel aufzuführen;\n"
+                         "Fragen einfach an meinen Erschaffer richten!\n\n"
+                         "Viel Spaß!")
+
+    return util.uwuify_by_chance(embed)
